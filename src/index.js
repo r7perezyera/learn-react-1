e1d4077e0b246ab455465e4d6c51e6ee0201bc0b
rtperezyera <tllezroberto9@gmail.com>
2020-10-17 16:36:59 -0500

    Add css to book project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import './index.css'
+
 
 function BookList() {
   return (
-    <section>
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
       <Book />
       <Book />
       <Book />
@@ -15,7 +21,7 @@ function BookList() {
 
 const Book = () => {
   return (
-  <article>
+  <article className='book'>
     <Image></Image>
     <Title />
     <Author />
