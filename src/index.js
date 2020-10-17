1fbccd162be57ae0fdcfb4af81b4bbe9baad09b2
rtperezyera <tllezroberto9@gmail.com>
2020-10-16 22:05:46 -0500

    Clean and update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+// ES6 modules, nothing specific to React
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Greet() {
+  // this essentially is a component (?)
+  return <h1><center>Hello world !!!</center></h1>;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// ReactDom has the render method
+ReactDom.render(<Greet />, document.getElementById('root'));
