a0a505bef5d8636b1091fb4d4d41fe93bda1d5f7
rtperezyera <tllezroberto9@gmail.com>
2020-10-17 14:07:48 -0500

    Move first lesson to separate file and clean index.js

--- src/01_appChunks.js
+++ src/01_appChunks.js
@@ -1,8 +1,11 @@
+// ES6 modules, nothing specific to React
 import React from 'react';
 import ReactDom from 'react-dom';
 
-
+// stateless functional component, aka dumb component
+// we must always return jsx
 function Greet() {
+  // this essentially is a component (?)
   return (
     <div>
       <Person />
@@ -12,13 +15,12 @@ function Greet() {
 }
 
 // with implicit return
-const Person = () => <h2>John Doe</h2>
+const Person = () => <h2>John Doe</h2>;
 
 // with explicit return
 const Message = () => {
   return <p>hello, am message</p>;
-}
-
+};
 
 const Greeting = () => {
   return React.createElement(
@@ -28,4 +30,5 @@ const Greeting = () => {
   );
 };
 
+// ReactDom has the render method
 ReactDom.render(<Greet />, document.getElementById('root'));
