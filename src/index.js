fccfc7e822d0d484722d24e49b7bffe13d16922c
rtperezyera <tllezroberto9@gmail.com>
2020-10-17 16:05:56 -0500

    Create book project with nested components

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 
-function Greet() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-// with implicit return
-const Person = () => <h2>John Doe</h2>
 
-// with explicit return
-const Message = () => {
-  return <p>hello, am message</p>;
-}
-
-
-const Greeting = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Hi there world!')
+const Book = () => {
+  return (
+  <article>
+    <Image></Image>
+    <Title />
+    <Author />
+  </article>
   );
 };
 
-ReactDom.render(<Greet />, document.getElementById('root'));
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg"
+    alt="Certification book"
+  />
+);
+
+const Title = () => <h2>CCNA 200-301 official cert guide</h2>;
+
+const Author = () => <h3>Wendel Odom</h3>;
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
