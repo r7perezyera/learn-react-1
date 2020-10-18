0a090bbe72bb2535357b76aa30851622c5ae0bf8
rtperezyera <tllezroberto9@gmail.com>
2020-10-18 15:54:35 -0500

    Add and play with JS within JSX

--- src/index.js
+++ src/index.js
@@ -13,15 +13,28 @@ function BookList() {
 }
 
 
+const author = 'Wendel Odom';
+
 const Book = () => {
+  
+  const title = 'CCNA 200-301 official cert guide!!';
+  
   return (
     <article className="book">
       <img
         src="https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg"
         alt="Certification book"
       />
-      <h2>CCNA 200-301 official cert guide</h2>
-      <h3>Wendel Odom</h3>
+      
+      {/* ONLY values here. NO JS statements */}
+      <h2>{title}</h2>
+      <h3>{author.toUpperCase()}</h3>
+      
+      {/* forbidden */}
+      {/* <p>{let x = 6}</p> */}
+
+      {/* this is OK, im returning a value */}
+      <p>{41 + 1}</p>
     </article>
   );
 };
