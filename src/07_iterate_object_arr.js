297c2db48acad7116827d24d794a455d37ec7ed3
rtperezyera <tllezroberto9@gmail.com>
2020-10-24 14:09:03 -0500

    Successfully iterate over array
    * In index file
    * Copied to file 07

--- src/07_iterate_object_arr.js
+++ src/07_iterate_object_arr.js
@@ -1,45 +1,44 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import './index.css'
+import './index.css';
 
 const books = [
   {
     title: 'CCNA 200-301 official cert guide!!',
     author: 'Wendel Odom',
-    imgUrl: 'https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg'
+    imgUrl: 'https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg',
   },
   {
     title: 'This is book 2',
     author: 'Author 2',
     imgUrl: 'https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg',
-  }
+  },
 ];
-
 
 function BookList() {
   return (
     // recall: here's where we RENDER the books
-    <section className='booklist'>{books.map((book) => {
-      const {title, author, img} = book;
-      
-      //console.log(book);
-      
-      // i can return whatever i want from the map method
-      /* return <div>
+    <section className="booklist">
+      {books.map((book) => {
+        const { title, author, img } = book;
+
+        //console.log(book);
+
+        // i can return whatever i want from the map method
+        /* return <div>
         <h3>{title}</h3>
         <h4>{author}</h4>
       </div> */
-      return (
-        <Book book={book}></Book>
-      );
-    })}</section>
+        return <Book book={book}></Book>;
+      })}
+    </section>
   );
 }
 
 const Book = (props) => {
-  console.log('someProp: ',props);
-  const {title, author, imgUrl} = props.book;
+  console.log('someProp: ', props);
+  const { title, author, imgUrl } = props.book;
   return (
     <article className="book">
       <img src={imgUrl} alt="Certification book" />
@@ -50,7 +49,5 @@ const Book = (props) => {
     </article>
   );
 };
-
-
 
 ReactDom.render(<BookList />, document.getElementById('root'));
