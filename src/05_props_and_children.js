418004756e2113f489063bb70c7523d2215a2290
rtperezyera <tllezroberto9@gmail.com>
2020-10-23 19:21:17 -0500

    Create file for props and children lesson

--- src/05_props_and_children.js
+++ src/05_props_and_children.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import './index.css'
-
+import './index.css';
 
 const book1 = {
   title: 'CCNA 200-301 official cert guide!!',
   author: 'Wendel Odom',
-  imgUrl: 'https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg'
-}
+  imgUrl: 'https://m.media-amazon.com/images/I/51kMtpaJQNL._SS135_.jpg',
+};
 const book2 = {
   title: 'CCNA 200-301 official cert guide!!',
   author: 'Wendel Odom',
@@ -18,22 +17,30 @@ const book2 = {
 function BookList() {
   return (
     // recall: here's where we RENDER the books
-    <section className='booklist'>
-      <Book 
+    <section className="booklist">
+      <Book
         title={book1.title}
         author={book1.author}
         imgUrl={book1.imgUrl}
         ans={42}
-        >
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates tempore commodi illo fugiat dicta alias aliquid ad harum, et voluptatum.</p>
+      >
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+          tempore commodi illo fugiat dicta alias aliquid ad harum, et
+          voluptatum.
+        </p>
       </Book>
-      <Book title="Hard-coded prop!" author={book2.author} imgUrl={book2.imgUrl} />
+      <Book
+        title="Hard-coded prop!"
+        author={book2.author}
+        imgUrl={book2.imgUrl}
+      />
     </section>
   );
 }
 
 const Book = (props) => {
-  const {title, author, imgUrl, ans, children} = props;
+  const { title, author, imgUrl, ans, children } = props;
   return (
     <article className="book">
       <img src={imgUrl} alt="Certification book" />
@@ -46,7 +53,5 @@ const Book = (props) => {
     </article>
   );
 };
-
-
 
 ReactDom.render(<BookList />, document.getElementById('root'));
